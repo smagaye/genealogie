55cc0400af89366b8bd281a02faab1dce1fb8bb9
Sma Gaye <sma.gaye94@gmail.com>
2018-11-03 21:50:19 +0000

    jar to war

--- src/main/java/com/smag/genealogie/GenealogieApplication.java
+++ src/main/java/com/smag/genealogie/GenealogieApplication.java
@@ -2,9 +2,15 @@ package com.smag.genealogie;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class GenealogieApplication {
+public class GenealogieApplication extends SpringBootServletInitializer {
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return super.configure(builder);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(GenealogieApplication.class, args);
